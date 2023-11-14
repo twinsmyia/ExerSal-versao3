fbfb053d14104a1f14d4acd89567f9f02f5e974f
Bruna Suelen <subr.garcia@gmail.com>
2023-11-14 19:12:00 -0300

    correção do erro de cors

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,13 @@
 import express from 'express';
 import routes from './index.js';
+import cors from 'cors';
 
 const app = express();
-app.use(express.json());
+
+//Implementando algumas configurações no projeto
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ extended: false }));
+app.use(cors({ origin: '*' }));
 
 routes(app);
 
